9257ef4b1c48baa6bebe46f845fdd745668d2a26
edwborges <eduardo.borges19646201@gmail.com>
2022-06-22 15:41:12 -0300

    snake game

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+import game.GameFrame;
+
+public class Program {
+
+	public static void main(String[] args) {
+		new GameFrame();
+	}
+
+}
---
+++ src/game/GameFrame.java
@@ -0,0 +1,17 @@
+package game;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	public GameFrame(){
+		this.add(new GamePanel());
+		this.setTitle("Snake Game");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+	}
+}
